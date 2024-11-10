631bcbd3ff6c3224037affc78e4e89273beeac65
zqi4869 <676768172@qq.com>
2024-11-10 13:32:44 +0800

    Complete add and edit and delete animal data

--- src/views/animals/Animals.jsx
+++ src/views/animals/Animals.jsx
@@ -1,26 +1,65 @@
 import React, { useState, useEffect } from 'react';
 import './Animals.css';
-import { SearchOutlined, EditOutlined, DeleteOutlined } from '@ant-design/icons';
+import { SearchOutlined, EditOutlined, DeleteOutlined, PlusOutlined } from '@ant-design/icons';
 import Uploader from "../../components/Uploader.jsx";
-import { Col, Row, Card, Button, Form, Input, message, Space, Table, Tag } from 'antd';
+import { Radio, Modal, Button, Form, Input, Checkbox, Space, Table, Tag } from 'antd';
+
 const { Column } = Table;
-import { fetchGet, getImageUri } from "../../utils/request.js";
+import { fetchGet, getImageUri, fetchPost } from "../../utils/request.js";
 
 const AnimalsPage = () => {
+  const [queryForm] = Form.useForm();
   const [form] = Form.useForm();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [initFormValues, setInitFormValues] = useState({});
   const [loginUser, setLoginUser] = useState();
   const [tableData, setTableData] = useState([]);
+  const [animalId, setAnimalId] = useState(null);
 
   const onQuery = () => {
-    const { no} = form.getFieldsValue()
+    const { no } = queryForm.getFieldsValue()
     fetchGet('/animal/all', data => {
-      if(no) {
+      if (no) {
         setTableData(data.filter(item => item.no.includes(no)))
-      }else{
+      } else {
         setTableData(data)
       }
     });
   };
+
+  const openDialog = () => {
+    setAnimalId(null);
+    setInitFormValues({});
+    form.resetFields();
+    setIsModalOpen(true);
+  };
+
+  const onEdit = (row) => {
+    setAnimalId(row.id);
+    setInitFormValues(row);
+    form.resetFields();
+    setIsModalOpen(true);
+  };
+
+  const onDelete = (row) => {
+    fetchPost('/animal/delete/' + row.id, {}, () => {
+      setIsModalOpen(false);
+      onQuery();
+    });
+  };
+
+  const onSubmit = () => {
+
+    fetchPost('/animal/save', {
+      ...form.getFieldsValue(),
+      id: animalId
+    }, () => {
+      setIsModalOpen(false);
+      onQuery();
+    });
+  };
+
+  const rules = [{ required: true, }]
 
   useEffect(() => {
     onQuery()
@@ -28,16 +67,68 @@ const AnimalsPage = () => {
 
   return (
     <>
-      <Form layout="inline" form={form}>
+      <Modal title="Basic animal information"
+             open={isModalOpen}
+             onOk={onSubmit}
+             onCancel={() => setIsModalOpen(false)}>
+        <Form
+          form={form}
+          name="basic"
+          labelCol={{
+            span: 5,
+          }}
+          initialValues={initFormValues}
+          autoComplete="off"
+        >
+          <Form.Item label="No" name="no" rules={rules}>
+            <Input/>
+          </Form.Item>
+          <Form.Item label="Name" name="name" rules={rules}>
+            <Input/>
+          </Form.Item>
+          <Form.Item label="Cover image" name="cover_url" rules={rules}>
+            <Uploader/>
+          </Form.Item>
+          <Form.Item label="Age" name="age" rules={rules}>
+            <Input/>
+          </Form.Item>
+          <Form.Item label="Gender" name="gender" rules={rules}>
+            <Radio.Group>
+              <Radio value="Male"> Male </Radio>
+              <Radio value="Female"> Female </Radio>
+            </Radio.Group>
+          </Form.Item>
+          <Form.Item label="City" name="city" rules={rules}>
+            <Input/>
+          </Form.Item>
+          <Form.Item label="Label" name="label" rules={rules}>
+            <Input placeholder="Multiple label Spaces separated"/>
+          </Form.Item>
+          <Form.Item label="Remark" name="remark">
+            <Input.TextArea rows={2}/>
+          </Form.Item>
+          <Form.Item label="Story" name="story">
+            <Input.TextArea rows={4}/>
+          </Form.Item>
+          <Form.Item label="Story image" name="story_img_url">
+            <Uploader/>
+          </Form.Item>
+        </Form>
+      </Modal>
+
+      <Form layout="inline" form={queryForm}>
         <Form.Item label="No" name="no">
-          <Input />
+          <Input/>
         </Form.Item>
         <Form.Item>
-          <Button icon={<SearchOutlined />} color="default" variant="solid" onClick={onQuery}>Query</Button>
+          <Space>
+            <Button icon={<SearchOutlined/>} color="default" variant="solid" onClick={onQuery}>Query</Button>
+            <Button icon={<PlusOutlined/>} color="default" variant="solid" onClick={openDialog}>Add</Button>
+          </Space>
         </Form.Item>
       </Form>
 
-      <br />
+      <br/>
 
       <Table dataSource={tableData} rowKey="id">
         <Column title="No." dataIndex="no" key="no"/>
@@ -45,7 +136,7 @@ const AnimalsPage = () => {
           title="Cover Image"
           dataIndex="cover_url"
           key="cover_url"
-          render={(url) => <img className="cover-img" src={getImageUri(url)} />}
+          render={(url) => <img className="cover-img" src={getImageUri(url)}/>}
         />
         <Column title="Name" dataIndex="name" key="name"/>
         <Column title="Age" dataIndex="age" key="age"/>
@@ -68,7 +159,7 @@ const AnimalsPage = () => {
           )}
         />
         <Column title="Remark" dataIndex="remark" key="remark"/>
-        <Column title="Story" dataIndex="story" key="story" />
+        <Column title="Story" dataIndex="story" key="story"/>
         <Column
           title="Adopted"
           dataIndex="adopted"
@@ -77,8 +168,8 @@ const AnimalsPage = () => {
             <>
               {
                 adopted ?
-                  <Tag color='green' style={{fontWeight: 'bold'}}>Yes</Tag> :
-                  <Tag color='volcano' style={{fontWeight: 'bold'}}>No</Tag>
+                  <Tag color='green' style={{ fontWeight: 'bold' }}>Yes</Tag> :
+                  <Tag color='volcano' style={{ fontWeight: 'bold' }}>No</Tag>
               }
             </>
           )}
@@ -88,8 +179,10 @@ const AnimalsPage = () => {
           key="action"
           render={(_, record) => (
             <Space>
-              <Button icon={<EditOutlined />} color="primary" variant="solid" size="small">Edit</Button>
-              <Button icon={<DeleteOutlined />} color="danger" variant="solid" size="small">Delete</Button>
+              <Button icon={<EditOutlined/>} color="primary" variant="solid" size="small"
+                      onClick={() => onEdit(record)}>Edit</Button>
+              <Button icon={<DeleteOutlined/>} color="danger" variant="solid" size="small"
+                      onClick={() => onDelete(record)}>Delete</Button>
             </Space>
           )}
         />
